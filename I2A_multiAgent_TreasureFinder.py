62aca8930da185c53a9003ee8b61626eb7a62ce4
ColdFrenzy <nemesi.95@hotmail.it>
2023-07-18 23:12:04 +0200

    Fixed Problems with dimensions
    The model received wrong shapes on initialization. ( Still have to check the rest of the code)

--- I2A_multiAgent_TreasureFinder.py
+++ I2A_multiAgent_TreasureFinder.py
@@ -158,9 +158,9 @@ class EnvFindTreasure(object):
             self.occupancy[self.half_pos][self.half_pos-1] = 0  # open secret door
             self.occupancy[self.half_pos][self.half_pos+1] = 0  # open secret door
         else:
-            self.occupancy[self.half_pos][self.half_pos] = 1  # open secret door
-            self.occupancy[self.half_pos][self.half_pos - 1] = 1  # open secret door
-            self.occupancy[self.half_pos][self.half_pos + 1] = 1  # open secret door
+            self.occupancy[self.half_pos][self.half_pos] = 1  # don't open secret door/close secret door
+            self.occupancy[self.half_pos][self.half_pos - 1] = 1  # don't open secret door/close secret door
+            self.occupancy[self.half_pos][self.half_pos + 1] = 1  # don't open secret door/close secret door
 
         # check treasure
         if self.agt1_pos == self.treasure_pos or self.agt2_pos == self.treasure_pos:
@@ -208,24 +208,24 @@ class EnvFindTreasure(object):
                     obs[i, j, 0] = 1.0
                     obs[i, j, 1] = 1.0
                     obs[i, j, 2] = 1.0
-                d_x = self.lever_pos[0] - self.agt1_pos[0]
-                d_y = self.lever_pos[1] - self.agt1_pos[1]
-                if d_x>=-1 and d_x<=1 and d_y>=-1 and d_y<=1:
-                    obs[1+d_x, 1+d_y, 0] = 1.0
-                    obs[1+d_x, 1+d_y, 1] = 1.0
-                    obs[1+d_x, 1+d_y, 2] = 0.0
-                d_x = self.treasure_pos[0] - self.agt1_pos[0]
-                d_y = self.treasure_pos[1] - self.agt1_pos[1]
-                if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
-                    obs[1+d_x, 1+d_y, 0] = 0.0
-                    obs[1+d_x, 1+d_y, 1] = 1.0
-                    obs[1+d_x, 1+d_y, 2] = 0.0
-                d_x = self.agt2_pos[0] - self.agt1_pos[0]
-                d_y = self.agt2_pos[1] - self.agt1_pos[1]
-                if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
-                    obs[1 + d_x, 1 + d_y, 0] = 0.0
-                    obs[1 + d_x, 1 + d_y, 1] = 0.0
-                    obs[1 + d_x, 1 + d_y, 2] = 1.0
+        d_x = self.lever_pos[0] - self.agt1_pos[0]
+        d_y = self.lever_pos[1] - self.agt1_pos[1]
+        if d_x>=-1 and d_x<=1 and d_y>=-1 and d_y<=1:
+            obs[1+d_x, 1+d_y, 0] = 1.0
+            obs[1+d_x, 1+d_y, 1] = 1.0
+            obs[1+d_x, 1+d_y, 2] = 0.0
+        d_x = self.treasure_pos[0] - self.agt1_pos[0]
+        d_y = self.treasure_pos[1] - self.agt1_pos[1]
+        if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
+            obs[1+d_x, 1+d_y, 0] = 0.0
+            obs[1+d_x, 1+d_y, 1] = 1.0
+            obs[1+d_x, 1+d_y, 2] = 0.0
+        d_x = self.agt2_pos[0] - self.agt1_pos[0]
+        d_y = self.agt2_pos[1] - self.agt1_pos[1]
+        if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
+            obs[1 + d_x, 1 + d_y, 0] = 0.0
+            obs[1 + d_x, 1 + d_y, 1] = 0.0
+            obs[1 + d_x, 1 + d_y, 2] = 1.0
         obs[1, 1, 0] = 1.0
         obs[1, 1, 1] = 0.0
         obs[1, 1, 2] = 0.0
@@ -239,24 +239,24 @@ class EnvFindTreasure(object):
                     obs[i, j, 0] = 1.0
                     obs[i, j, 1] = 1.0
                     obs[i, j, 2] = 1.0
-                d_x = self.lever_pos[0] - self.agt2_pos[0]
-                d_y = self.lever_pos[1] - self.agt2_pos[1]
-                if d_x>=-1 and d_x<=1 and d_y>=-1 and d_y<=1:
-                    obs[1+d_x, 1+d_y, 0] = 1.0
-                    obs[1+d_x, 1+d_y, 1] = 1.0
-                    obs[1+d_x, 1+d_y, 2] = 0.0
-                d_x = self.treasure_pos[0] - self.agt2_pos[0]
-                d_y = self.treasure_pos[1] - self.agt2_pos[1]
-                if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
-                    obs[1+d_x, 1+d_y, 0] = 0.0
-                    obs[1+d_x, 1+d_y, 1] = 1.0
-                    obs[1+d_x, 1+d_y, 2] = 0.0
-                d_x = self.agt1_pos[0] - self.agt2_pos[0]
-                d_y = self.agt1_pos[1] - self.agt2_pos[1]
-                if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
-                    obs[1 + d_x, 1 + d_y, 0] = 1.0
-                    obs[1 + d_x, 1 + d_y, 1] = 0.0
-                    obs[1 + d_x, 1 + d_y, 2] = 0.0
+        d_x = self.lever_pos[0] - self.agt2_pos[0]
+        d_y = self.lever_pos[1] - self.agt2_pos[1]
+        if d_x>=-1 and d_x<=1 and d_y>=-1 and d_y<=1:
+            obs[1+d_x, 1+d_y, 0] = 1.0
+            obs[1+d_x, 1+d_y, 1] = 1.0
+            obs[1+d_x, 1+d_y, 2] = 0.0
+        d_x = self.treasure_pos[0] - self.agt2_pos[0]
+        d_y = self.treasure_pos[1] - self.agt2_pos[1]
+        if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
+            obs[1+d_x, 1+d_y, 0] = 0.0
+            obs[1+d_x, 1+d_y, 1] = 1.0
+            obs[1+d_x, 1+d_y, 2] = 0.0
+        d_x = self.agt1_pos[0] - self.agt2_pos[0]
+        d_y = self.agt1_pos[1] - self.agt2_pos[1]
+        if d_x >= -1 and d_x <= 1 and d_y >= -1 and d_y <= 1:
+            obs[1 + d_x, 1 + d_y, 0] = 1.0
+            obs[1 + d_x, 1 + d_y, 1] = 0.0
+            obs[1 + d_x, 1 + d_y, 2] = 0.0
         obs[1, 1, 0] = 0.0
         obs[1, 1, 1] = 0.0
         obs[1, 1, 2] = 1.0
@@ -266,6 +266,7 @@ class EnvFindTreasure(object):
         return [self.get_agt1_obs(), self.get_agt2_obs()]
 
     def get_state(self):
+
         state = np.zeros((1, 4))
         state[0, 0] = self.agt1_pos[0] / self.map_size
         state[0, 1] = self.agt1_pos[1] / self.map_size
@@ -320,24 +321,19 @@ class EnvironmentModel(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(EnvironmentModel, self).__init__()
         self.state_dim = state_dim
-        self.action_dim = action_dim
+        self.action_dim = 1
 
-        self.fc1 = nn.Linear(state_dim[0] + action_dim[0], 32)
-        self.fc2 = nn.Linear(32, 32)
-        self.state_head = nn.Linear(32, state_dim[0])
+        self.fc1 = nn.Linear(self.state_dim + self.action_dim, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 32)
+        self.state_head = nn.Linear(32, self.state_dim)
 
     def forward(self, state, action):
-        print("State shape before flattening:", state.shape)
-        state = state.squeeze(0)  # Remove the batch dimension
-        print("State shape after squeezing:", state.shape)
-        state = state.permute(2, 0, 1)  # Reshape to (3, 7, 7)
-        print("State shape after permuting:", state.shape)
-        state = state.view(1, state.size(0), state.size(1), state.size(2))  # Add the batch dimension back
-        print("State shape after reshaping:", state.shape)
-        action = action.view(action.size(0), -1)  # Flatten the action
         state_action = torch.cat([state, action], dim=-1)
+        state_action = state_action.to(torch.float32)
         x = F.relu(self.fc1(state_action))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         next_state = self.state_head(x)
         return next_state
 
@@ -346,16 +342,16 @@ class EnvironmentModel(nn.Module):
 class I2A_FindTreasure(nn.Module):
     def __init__(self, state_dim, action_dim, rollout_len=5, hidden_dim=256):
         super(I2A_FindTreasure, self).__init__()
-        self.state_dim = state_dim
-        self.action_dim = action_dim
+        self.state_dim = np.prod(state_dim)
+        self.action_dim = action_dim[0]
         self.rollout_len = rollout_len
 
         # Define the environment model
-        self.env_model = EnvironmentModel(state_dim, action_dim)
+        self.env_model = EnvironmentModel(self.state_dim, self.action_dim)
 
         # Define the imagination module (rollout encoders)
         self.imagination = nn.Sequential(
-            nn.Linear(state_dim[0] * rollout_len, hidden_dim),
+            nn.Linear(self.state_dim * rollout_len, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
@@ -372,24 +368,23 @@ class I2A_FindTreasure(nn.Module):
 
     def forward(self, state, action_space):
         # Check if the state has the correct shape
-        if state.size(1) != 7 or state.size(2) != 7 or state.size(3) != 3:
+        if state.shape[1] != 3 or state.shape[2] != 7 or state.shape[3] != 7:
             print("irregular state shape before passing to the env model:", state.shape)
             raise ValueError("Invalid state dimension. Expected shape: (7, 7, 3)")
 
         # Flatten the state tensor
-        state = state.view(state.size(0), -1)
+        if isinstance(state, np.ndarray):
+            state = torch.from_numpy(state)
+        state = torch.reshape(state , (state.shape[0], -1))
 
         # Pass the flattened state to the imagination module
         imagined_states = []
         for _ in range(self.rollout_len):
             # Generate a random action
-            action = torch.randint(action_space, (state.size(0), 1))
-
-            # Concatenate the state and action
-            state_action = torch.cat([state, action], dim=-1)
+            action = torch.randint(int(action_space), (state.shape[0], 1))
 
             # Pass the concatenated state-action to the environment model
-            next_state = self.env_model(state_action)
+            next_state = self.env_model(state, action)
 
             # Store the next state in the imagined states
             imagined_states.append(next_state)
@@ -484,7 +479,6 @@ def select_action(model, state, output_size, epsilon):
         action = torch.randint(0, output_size[0], (1,))
     else:
         # Use I2A module to produce action
-        state = torch.tensor(state, dtype=torch.float32).permute(0, 3, 1, 2)  # Reshape state tensor to (1, 3, 7, 7)
         action_space = torch.tensor([output_size[0]], dtype=torch.float32).unsqueeze(0)
 
         with torch.no_grad():
@@ -522,7 +516,7 @@ def main(args):
     # Main training loop
     for episode in range(args.num_episodes):
         state = env.reset()
-        print("reset enviro:", state.shape)
+        print("reset environment:", state.shape)
         episode_reward_agent1 = 0
         episode_reward_agent2 = 0
 
@@ -536,7 +530,7 @@ def main(args):
             action_list= [action_agent1, action_agent2]
             reward, done = env.step(action_list)
 
-
+            
             next_state1 = env.get_global_obs()
             next_state1 = np.transpose(next_state1, (2, 0, 1))  # Transpose dimensions to (channels, height, width)
             next_state1 = np.expand_dims(next_state1, axis=0)  # Add an extra dimension for the batch
